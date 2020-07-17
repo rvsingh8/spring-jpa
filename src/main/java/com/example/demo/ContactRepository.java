b3372d0203ef5fa156d82b94a9b99c95b0aca505
Vivek Singh <vivek.singh@coveiot.com>
2020-07-17 20:58:53 +0530

    Finished all the tasks

--- src/main/java/com/example/demo/ContactRepository.java
+++ src/main/java/com/example/demo/ContactRepository.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ContactRepository extends CrudRepository<Contact, Integer> {
+	
+
+	List<Contact> findByNameOrEmail(@Param("name") String value, @Param("email") String value1);
 
 }
--- src/main/java/com/example/demo/MyRESTController.java
+++ src/main/java/com/example/demo/MyRESTController.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 public class MyRESTController {
@@ -30,5 +34,15 @@ public class MyRESTController {
 		}
 		return "Contact Saved Successfully";
 	}
+	
+	@GetMapping("/search")
+	public ResponseEntity getItem(@RequestParam("query") String query) {
+		if(query != null) {
+			List<Contact> list = repository.findByNameOrEmail(query, query);
+			return new ResponseEntity<>(list, HttpStatus.OK);
+		}
+		return new ResponseEntity<>("Contact Not Found",HttpStatus.OK);
+	}
+	
 
 }
